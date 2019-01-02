2625af526206a7f4895a31a1a55ad4a3d7c00c03
praveen <praveen.vundyala@gmail.com>
2019-01-02 22:30:49 +0530

    fetching contacts from default contacts app

--- ContactsDemo/ViewController.swift
+++ ContactsDemo/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CNContactPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+//        fetchContacts()
+        
+        let picker = CNContactPickerViewController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    // use this to pick single contact
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        print(contact.phoneNumbers.first?.value.stringValue)
+    }
+    
+    // use this to pick multi contacts
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contacts: [CNContact]) {
+        contacts.forEach { (contact) in
+            for data in contact.phoneNumbers {
+                let phoneNo = data.value
+                print(phoneNo.stringValue)
+
+            }
+        }
     }
 
-
+    // use this to fetch totals contacts data to app to display and customize
+    func fetchContacts() {
+        let store = CNContactStore()
+        store.requestAccess(for: .contacts) { (granted, error) in
+            if let err = error {
+                print("failed to access", err)
+                return
+            }
+            if granted {
+                print("granted...")
+                let keys = [CNContactGivenNameKey]
+                let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+                do {
+                    try store.enumerateContacts(with: request, usingBlock: { (contact, stopPointer) in
+                        print(contact.givenName)
+                    })
+                } catch let err {
+                    print(err)
+                }
+            } else {
+                print("denied...")
+            }
+        }
+    }
 }
 
